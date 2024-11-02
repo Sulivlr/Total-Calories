6a51059c49e240672c04ac999be0d3cc98fef3b6
Sultan Mukhtarov <suli419956@gmail.com>
2024-11-02 17:47:13 +0600

    created two-way binding.

--- src/Containers/NewMeal/NewMeal.tsx
+++ src/Containers/NewMeal/NewMeal.tsx
@@ -1,8 +1,76 @@
+import React, {useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {Meals} from '../../constants';
+
 const NewMeal = () => {
+  const [meal, setMeal] = useState({
+    time: '',
+    description: '',
+    calories: '',
+  });
+
+  const onFieldChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    const {name, value} = event.target;
+    setMeal((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const {mealId} = useParams();
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(meal);
+  };
+
   return (
-    <div>
-      New Meal
-    </div>
+    <>
+      <h4>{mealId ? 'Edit' : 'Add meal'}</h4>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="time">Select a Meal</label>
+          <select
+            value={meal.time}
+            onChange={onFieldChange}
+            name="time"
+            id="time"
+            className="form-select"
+          >
+            <option value="">Select a Meal.....</option>
+            {Meals.map((meal) => (
+              <option key={meal.url} value={meal.url}>{meal.name}</option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="time">Description</label>
+          <textarea
+            value={meal.description}
+            onChange={onFieldChange}
+            name="description"
+            id="description"
+            className="form-control"
+            required
+          />
+          <div className="form-group">
+            <label htmlFor="time">Calories</label>
+            <input
+              value={meal.calories}
+              onChange={onFieldChange}
+              name="calories"
+              id="calories"
+              className="form-control"
+              required
+            />
+          </div>
+          <button
+            className="btn btn-success mt-2"
+            type="submit"
+            disabled={false}
+            >
+            Save
+          </button>
+        </div>
+
+      </form>
+    </>
   );
 };
 
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const Meals = [
+  {name: 'Breakfast', url: 'breakfast'},
+  {name: 'Snack', url: 'snack'},
+  {name: 'Lunch', url: 'lunch'},
+  {name: 'Dinner', url: 'dinner'},
+];
